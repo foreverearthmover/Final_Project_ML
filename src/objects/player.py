1be4cce39678785748d93eb953691f37e89ac477
MajaPapayo <maja.f.menzel@stud.leuphana.de>
2025-07-22 17:40:31 +0200

    reorganising __init__

--- src/objects/player.py
+++ src/objects/player.py
@@ -1,24 +1,20 @@
 import pygame
 import os
-from pygame.examples.sprite_texture import sprite
-
-#this needs to be somewhere later not sure if it can stay here
 
 WHITE = (255, 255, 255)
-#putting a BG to work, later DELETE
 
-# Setting game window dimensions
+#putting a BG to work, later DELETE
 window_width = 800
 window_height = 600
 screen = pygame.display.set_mode((window_width, window_height))
+
+#Load BG DELETE
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_path = os.path.join(BASE_DIR, "..", "..", "assets", "media", "backgrounds", "garden.png")
-image_path = os.path.normpath(image_path)
-
 background = pygame.image.load(image_path)
 
 
-# it is having troubles t find the picture, so I used this method, but i dont know where to put it in the code yet:
+#Load sprite
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sprite_sheet_path = os.path.join(BASE_DIR, "..", "..", "assets", "media", "sprites", "cat_sprite.png")
 sprite_sheet = pygame.image.load(sprite_sheet_path).convert_alpha()
@@ -27,12 +23,20 @@ sprite_sheet = pygame.image.load(sprite_sheet_path).convert_alpha()
 
 
 class Cat:
-    def __init__(self,sheet, frame, width, height, x, y, scale, color):
+    def __init__(self, sheet, frame_width, frame_height, x, y, scale, color, num_frames):
         self.sprite_sheet = sheet
-        self.image = self.get_image(sprite_sheet,((frame * width) , x,y,width, height, scale, color))
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+        self.scale = scale
+        self.color = color
+        self.frames = []
+        self.num_frames = num_frames
+        self.load_frames()
+        self.frame_index = 0
+        self.image = self.frames[self.frame_index]
         self.rect = self.image.get_rect(topleft=(x, y))
         self.speed = 4
-        self.frame = self.get_image(sprite_sheet,((frame * width),0, 0, 900,900, 0.2, WHITE))
+
 
 
     #printing the image on a different sheet
