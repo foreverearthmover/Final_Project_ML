3d81837714e4b44b05aa6b53b014bc20316a847a
MajaPapayo <maja.f.menzel@stud.leuphana.de>
2025-07-23 15:19:40 +0200

    finished the dictonairy!

--- src/objects/item.py
+++ src/objects/item.py
@@ -9,12 +9,24 @@ inventory = []
 
 #dictonary of what is inside the rooms
 rooms = {
-    "living room":[
-        {"item": "Cat tree", "movability": "no", "use": "attack boost", "msg": "you scratch your claws on the tree."},
-        {"item": "Couch", "movability": "no", "use": "health", "msg": "you lie down on the couch and take a nap."},
-        {"item": "Food bowl", "movability": "no", "use": "none", "msg": "the bowl is empty, but you are still hungry."},
-        {"item": "Carton", "movability": "yes", "use": "attack", "msg": "These look knotted, be careful to not get caught."},
-        { }#do we add a message?
+    "Living room":[
+        {"item": "Cat tree", "movable": "no", "use": "attack boost", "msg": "you scratch your claws on the tree."},
+        {"item": "Couch", "movable": "no", "use": "health", "msg": "you lie down on the couch and take a nap."},
+        {"item": "Food bowl", "movable": "no", "use": "none", "msg": "the bowl is empty, but you are still hungry."},
+        {"item": "Cable", "movable": "yes", "use": "attack", "msg": "These look knotted, be careful to not get caught."},
+        {"item": "Cartoon", "movable": "yes", "use": "key ", "msg": "You could go inside, or maybe on top?"},
+        {"item": "Yarn ball", "movable": "yes", "use": "attack", "msg": "That looks fun! But lets not get distracted right now."},
+        #do we add a message?
+    ],
+    "Bathroom":[
+        {"item": "Toilet", "movable": "no", "use": "none", "msg": "That is a Toilet."},
+        {"item": "Shower", "movable": "no", "use": "none", "msg": "She is still in the shower, but you can't wait to eat."},
+        {"item": "Cat litter", "movable": "yes", "use": "none", "msg": "I dont't need to go right now."},
+        {"item": "Toilet paper", "movable": "yes", "use": "attack", "msg": "You could roll down the entire roll.. Or maybe just take one."},
+    ],
+    "Garden":[
+        {"item": "Squirrel", "movable": "no", "use": "Scene change", "msg": "You could try to catch that Squirrel!"},
+        {"item": "Boss Cat", "movable": "no", "use": "interact", "msg": "Other cat bad."},
     ]
 }
 
